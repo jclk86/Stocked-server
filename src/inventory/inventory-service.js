c684f214f77112552ae48392c9bc479d9209d0a7
John Chan <johnchancontact@gmail.com>
2019-09-08 14:00:16 -0400

    implemented post and patch routes

--- src/inventory/inventory-service.js
+++ src/inventory/inventory-service.js
@@ -25,6 +25,7 @@ const InventoryService = {
   },
   getByUserIdAndItemId(db, user_id, item_id) {
     // return db.from("stocked_items").select("*").where("id", id).first();
+    //not sure if you actually need to get userId with all of this, when likely, the db you fetched only includes the user's inventory anyways.
     return db
     .from("user_items AS u")
     .join("stocked_users AS su", "u.user_id", "=", "su.id")
@@ -45,8 +46,39 @@ const InventoryService = {
       "st.name"
     )
     .where("u.item_id", item_id)
-    .andWhere("u.user_id", user_id)
+    .andWhere("u.user_id", user_id);
+  },
+  // Note: you may pmay not need to incorporate the userId for this. 
+  updateItem(db, id, newItemFields) {
+    return db.from("stocked_items")
+    .where({id})
+    .update(newItemFields);
+  },
+  deleteItem(db, id) {
+    return db.from({id}).del();
+  },
+  insertItem(db, newItem) {
+    return db.insert(newItem).into("stocked_items").returning("*")
+    .then(rows => {
+      return rows[0]
+    });
+  },
+  // make sure this is actually accepting an object or if it needs to 
+  // also, keep an eye on the "desc" if it needs the quotations or not 
+  serializeItem(item) {
+    return {
+      id: item.id,
+      name: item.name,
+      desc: item.desc,
+      image_url: item.image_url,
+      quantity: item.quantity,
+      cost: item.cost,
+      date_modified: item.date_modified,
+      unit: item.unit,
+      tag: item.tag
+    }
   }
+  
 };
 
 module.exports = InventoryService;
